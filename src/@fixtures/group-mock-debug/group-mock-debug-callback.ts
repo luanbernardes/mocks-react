16628c17bb4d34f0dbd2655fbc8a8cdb79bf1067
Luan <luan.bernardes@tino.com.br>
2024-03-14 21:01:41 -0300

    feat: remove TODOs

--- src/@fixtures/group-mock-debug/group-mock-debug-callback.ts
+++ src/@fixtures/group-mock-debug/group-mock-debug-callback.ts
@@ -3,14 +3,14 @@ import { GroupMockDebug } from '../../@types';
 export const groupMockDebugsCallbackMock: GroupMockDebug[] = [
   {
     title: 'TESTE',
-    type: 'callback', // TODO no componente usa esse aqui
+    type: 'callback',
     list: [
       {
         title: 'mock-test-MSW',
         path: '/mock-test-MSW',
         method: 'get',
         delay: 1000,
-        type: 'callback', // TODO faz sentido esse callback aqui?
+        type: 'callback',
         options: [
           {
             id: 'mock-bill-success',
